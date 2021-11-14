87854803f0cf43feed940a8af13be350be31b06e
duymilo49 <94276667+duymilo49@users.noreply.github.com>
2021-11-14 13:00:31 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -7,8 +7,8 @@ window.addEventListener('load', () => {
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes',
-    cancelButtonText: 'No',
+    confirmButtonText: 'Có',
+    cancelButtonText: 'Không',
   }).then((result) => {
     if (result.isConfirmed) {
       document.querySelector('.song').play();
